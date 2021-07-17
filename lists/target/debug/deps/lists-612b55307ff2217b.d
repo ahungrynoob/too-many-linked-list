01a1d9f6638fe81811df460a28b775178d82a958
ahungrynoob <dxd_sjtu@outlook.com>
2021-07-17 23:23:51 +0800

    .gitignore is now working

--- lists/target/debug/deps/lists-612b55307ff2217b.d
+++
@@ -1,6 +0,0 @@
-/Users/bytedance/Opensource/too-many-linked-list/lists/target/debug/deps/lists-612b55307ff2217b.rmeta: src/lib.rs src/first.rs
-
-/Users/bytedance/Opensource/too-many-linked-list/lists/target/debug/deps/lists-612b55307ff2217b.d: src/lib.rs src/first.rs
-
-src/lib.rs:
-src/first.rs:
